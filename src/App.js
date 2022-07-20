071667081101117e53c200466af598c9a39bf103
Juan <juancarlos.gonzalezhuerta@aol.com>
2022-07-19 19:55:35 -0500

    added needed packages and error corrections

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
   return (
     <div className=''>
-      <nav className="bg-blue-100 border-black px-200 sm:px-40 py-2.5 rounded-b-xl dark:bg-gray-900 ">
+      <nav className="bg-red-100 border-black px-200 sm:px-40 py-2.5 rounded-b-xl dark:bg-gray-900 ">
         <div className="container flex flex-wrap justify-between items-center mx-auto">
           <div className='flex w-96 justify-around'>
             <div className='hover:text-white text-black-500 transition hover:animate-pulse font-semibold cursor-pointer'>
@@ -36,19 +36,19 @@ function App() {
         <br></br>
         
         
-        <h2 className='text-4xl text-white text-center font-extrabold'>Juan Carlos Gonzalez Huerta</h2>
+        <h2 className='text-4xl text-white font-extrabold'>Juan Carlos Gonzalez Huerta</h2>
         <br></br>
-        <p className='text-xl text-center  text-gray-400'>{t("body.descripcion")}</p>
+        <p className='text-xl text-gray-400'>{t("body.descripcion")}</p>
         <br></br>
         <br></br>
-        <button  onClick={()=>{setCount(count + 1)}} class="bg-blue-500 text-center  hover:bg-red-700 text-black font-bold py-5 px-12 border border-red-700 rounded">
+        <button  onClick={()=>{setCount(count + 1)}} class="bg-red-500 hover:bg-red-700 text-black font-bold py-5 px-12 border border-red-700 rounded">
         {t("body.contador")}
         </button>
-        <p className='text-center text-white'>{t("body.contador2")}: {count}</p>
+        <p className=' text-white'>{t("body.contador2")}: {count}</p>
         <br></br>
-        <p className='text-xl text-center text-white'>{t("body.DescripcionTecnologias")}</p>
+        <p className='text-xl text-white'>{t("body.DescripcionTecnologias")}</p>
         
-        <p className='text-xl text-center text-white'>{t("body.tecnologias")}</p>
+        <p className='text-xl text-white'>{t("body.tecnologias")}</p>
 
         </div>
      
